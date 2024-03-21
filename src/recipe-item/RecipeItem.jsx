7e51906ac705d63d1ec04734299566e46f2e0e1b
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-03-21 22:46:20 +0300

    Add custom hook useActions.

--- src/recipe-item/RecipeItem.jsx
+++ src/recipe-item/RecipeItem.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import styles from "./RecipeItem.module.scss";
-import { useDispatch } from "react-redux";
-import { actions } from "../store/favorites/favorites";
+import { useDispatch, useSelector } from "react-redux";
+import { actions } from "../store/favorites/favorites.slice";
+import { useActions } from "../hooks/useActions";
 
 const RecipeItem = ({ recipe }) => {
-  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites);
+
+  const isInFavorites = favorites.some((r) => r.id === recipe.id);
+
+  // const dispatch = useDispatch();
+  const { toggleFavourites } = useActions();
+  console.log("🚀 ~ RecipeItem ~ actions:", actions);
 
   return (
     <div className={styles.item}>
       <h3>{recipe.name}</h3>
-      <button onClick={() => dispatch(actions.toggleFavourites(recipe))}>
-        Add Top
+      <button onClick={() => toggleFavourites(recipe)}>
+        {isInFavorites ? "Remove from" : "Add to"} favorites
       </button>
     </div>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { reducer as favoriteReducer } from "./favorites/favorites";
+import { reducer as favoriteReducer } from "./favorites/favorites.slice";
 
 const reducers = combineReducers({
   favorites: favoriteReducer,
